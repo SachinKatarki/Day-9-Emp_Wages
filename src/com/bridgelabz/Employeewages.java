71254ff7726b20a42da748c14d597dd14422e006
Textspot <sachinkatarki1995@gmail.com>
2021-11-10 12:28:22 +0530

    Added:Welcome to Empwages for multiple companies programme

--- src/com/bridgelabz/Employeewages.java
+++ src/com/bridgelabz/Employeewages.java
@@ -1,26 +1,27 @@
 package com.bridgelabz;
-import jdk.swing.interop.SwingInterOpUtils;
+
 
 public class Employeewages {
-	public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int IS_FULL_TIME = 1;
-    public static final int IS_PART_TIME = 2;
-    public static int MAX_WORKING_DAYS = 20;
-    public static final int MAX_WORKING_HRS = 100;
+	 public static final int EMP_RATE_PER_HOUR = 20;
+	    public static final int IS_FULL_TIME = 1;
+	    public static final int IS_PART_TIME = 2;
+	    public static int MAX_WORKING_DAYS = 20;
+	    public static final int MAX_WORKING_HRS = 100;
 
 
 	public static void main(String[] args) {
 		 System.out.println("Welcome to the Employee Wage Computation Program !!");
-	        EmployeeWageComp();
+	        Employeewages  company1 =new Employeewages( );
+	        EmployeeWageComp("Dmart",20,20,100);
+	        Employeewages  company2 =new Employeewages( );
+	        EmployeeWageComp("Kmart",22,20,70);
+	        Employeewages  company3=new Employeewages( );
+	        EmployeeWageComp("Bridgelabz",15,2,20);
 	    }
-	    public static void EmployeeWageComp(){
-	        int workingDay = 20;
-	        int empHrs = 0;
-	        int empwage = 0;
-	        int totalWage = 0;
-	        int totalWorkingHrs = 0;
-	        int totalWorkingDays = 0;
-	        while (totalWorkingHrs <= MAX_WORKING_HRS && totalWorkingHrs < MAX_WORKING_DAYS) {
+
+	    public static int EmployeeWageComp(String company, int empRatePerHr, int numWorkingDays, int maxHrs) {
+	        int empHrs = 0, totalEmpHrs = 8, totalWorkingDays = 0;
+	        while (totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays < MAX_WORKING_DAYS) {
 	            totalWorkingDays++;
 	            int empcheck = (int) (Math.floor(Math.random() * 10) % 3);
 	            switch (empcheck) {
@@ -36,14 +37,14 @@ public class Employeewages {
 	                    System.out.println("Employee is Absent");
 	                    empHrs = 0;
 	            }
-	            totalWorkingHrs += empHrs;
-	            empwage = empHrs * EMP_RATE_PER_HOUR;
-	            System.out.println(" Employee Wage = " + empwage);
-	            totalWage = totalWage + empwage;
-	            System.out.println("Total working hrs = " + totalWorkingHrs);
-	            System.out.println("Total Emp wage for 20 days = " + totalWage);
+	            totalEmpHrs += empHrs;
+	        }
+	            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+	            System.out.println(company + " company Total Daily Wage for days " + totalWorkingDays + " and hours " + totalEmpHrs + " is: " + totalEmpWage);
+
+	            return totalEmpWage;
 	        }
 	}
 
-}
+
 
