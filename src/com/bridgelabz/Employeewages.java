2d99d53bf9c0b4cb0bd317dfcc247cfc0b72a3d9
Textspot <sachinkatarki1995@gmail.com>
2021-11-10 11:20:25 +0530

    Added:Welcome to Emp_Present_Abesent programme

--- src/com/bridgelabz/Employeewages.java
+++ src/com/bridgelabz/Employeewages.java
@@ -1,10 +1,19 @@
 package com.bridgelabz;
+import jdk.swing.interop.SwingInterOpUtils;
 
 public class Employeewages {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to the Employee Wage Computation Program !!");
+        int IS_FULL_TIME = 1;
+        double empcheck = Math.floor(Math.random() * 10) % 2;
+        if (empcheck == IS_FULL_TIME) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
 		
 
 	}
 
+}
 }
